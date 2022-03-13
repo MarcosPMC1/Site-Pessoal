0799e6abbf7714513d598e93e9dd4231d860f3e3
Marcos <marcospmc@outlook.com>
2022-03-12 21:50:11 -0300

    links erro

--- src/components/portifolio/Portifolio.jsx
+++ src/components/portifolio/Portifolio.jsx
@@ -9,62 +9,62 @@ const Portifolio = () => {
       <div className='container portifolio__container'> 
         <article className='portifolio__item'>
           <div className='portifolio__item-image'>
-            <img src='../../assets/portifolio.png' />
+            <img src='../../assets/portifolio.png' alt='Imagem Portifolio'  />
           </div>
           <h3>Projeto #1</h3>
           <div className='portifolio__links'>
-            <a href="#" className='btn' target='_blank'>Github</a>
-            <a href="#" className='btn btn-primary' target='_blank'>Demo</a>
+            <a href="#header" className='btn' target='_blank'>Github</a>
+            <a href="#header" className='btn btn-primary' target='_blank'>Demo</a>
           </div>
         </article>
         <article className='portifolio__item'>
           <div className='portifolio__item-image'>
-            <img src='../../assets/portifolio.png' />
+            <img src='../../assets/portifolio.png' alt='Imagem Portifolio'  />
           </div>
           <h3>Projeto #2</h3>
           <div className='portifolio__links'>
-            <a href="#" className='btn' target='_blank'>Github</a>
-            <a href="#" className='btn btn-primary' target='_blank'>Demo</a>
+            <a href="#header" className='btn' target='_blank'>Github</a>
+            <a href="#header" className='btn btn-primary' target='_blank'>Demo</a>
           </div>
         </article>
         <article className='portifolio__item'>
           <div className='portifolio__item-image'>
-            <img src='../../assets/portifolio.png' />
+            <img src='../../assets/portifolio.png' alt='Imagem Portifolio'  />
           </div>
           <h3>Projeto #3</h3>
           <div className='portifolio__links'>
-            <a href="#" className='btn' target='_blank'>Github</a>
-            <a href="#" className='btn btn-primary' target='_blank'>Demo</a>
+            <a href="#header" className='btn' target='_blank'>Github</a>
+            <a href="#header" className='btn btn-primary' target='_blank'>Demo</a>
           </div>
         </article>
         <article className='portifolio__item'>
           <div className='portifolio__item-image'>
-            <img src='../../assets/portifolio.png' />
+            <img src='../../assets/portifolio.png' alt='Imagem Portifolio'  />
           </div>
           <h3>Projeto #3</h3>
           <div className='portifolio__links'>
-            <a href="#" className='btn' target='_blank'>Github</a>
-            <a href="#" className='btn btn-primary' target='_blank'>Demo</a>
+            <a href="#header" className='btn' target='_blank'>Github</a>
+            <a href="#header" className='btn btn-primary' target='_blank'>Demo</a>
           </div>
         </article>
         <article className='portifolio__item'>
           <div className='portifolio__item-image'>
-            <img src='../../assets/portifolio.png' />
+            <img src='../../assets/portifolio.png' alt='Imagem Portifolio' />
           </div>
           <h3>Projeto #3</h3>
           <div className='portifolio__links'>
-            <a href="#" className='btn' target='_blank'>Github</a>
-            <a href="#" className='btn btn-primary' target='_blank'>Demo</a>
+            <a href="#header" className='btn' target='_blank'>Github</a>
+            <a href="#header" className='btn btn-primary' target='_blank'>Demo</a>
           </div>
         </article>
         <article className='portifolio__item'>
           <div className='portifolio__item-image'>
-            <img src='../../assets/portifolio.png' />
+            <img src='../../assets/portifolio.png' alt='Imagem Portifolio' />
           </div>
           <h3>Projeto #3</h3>
           <div className='portifolio__links'>
-            <a href="#" className='btn' target='_blank'>Github</a>
-            <a href="#" className='btn btn-primary' target='_blank'>Demo</a>
+            <a href="#header" className='btn' target='_blank'>Github</a>
+            <a href="#header" className='btn btn-primary' target='_blank'>Demo</a>
           </div>
         </article>
       </div>
